1b6579b857c49ce59d524d9f84558121dc47919f
falcao00 <nickbartowskireserva@gmail.com>
2024-10-04 00:20:07 -0300

    bg03 - adicionando funcao teste pra jogar conteudo de uma pagina pra outra, ainda em contrucao

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/* chamado contedo para poder testar */
+upContent();
+
+
+/* functions */
 function getTitulo(){
     const dataAtual = new Date();
     const options = { timeZone: 'America/Sao_Paulo' };
@@ -13,4 +18,59 @@ function getTitulo(){
     console.log(data);
     console.log(text);
     console.log(dataHoraBrasil);
+
+    localStorage.setItem("titulo", titulo);
+    localStorage.setItem("data", dataHoraBrasil);
+    localStorage.setItem("text", text);
+}
+
+function upContent(){ /* A ideia é subir o feed da home primeiro e poder acessar */
+    
+    let feed = document.getElementById("feed-home");
+    let post = document.getElementById("post-home");
+    let titulo = document.getElementById("titulo-post-home");
+    let data = document.getElementById("data-post-home");
+    let conteudo = document.getElementById("content-post-home");
+    localStorage.setItem("feed-home", feed); /* aqui eu tenho o htmlELEMENT */
+    localStorage.setItem("post-home", post); /* aqui eu tenho o htmlELEMENT */
+    localStorage.setItem("titulo-post-home", titulo); /* aqui eu tenho o htmlELEMENT */
+    localStorage.setItem("data-post-home", data); /* aqui eu tenho o htmlELEMENT */
+    localStorage.setItem("content-post-home", conteudo); /* aqui eu tenho o htmlELEMENT */
+    let feedobject = feed;
+    let postobject = post;
+    let tituloobj = titulo;
+    let dataobj = data;
+    let conteudobj = conteudo;
+    //console.log(feedobject.outerHTML);
+    //console.log(postobject.outerHTML);
+    //console.log(tituloobj.outerHTML);
+    //console.log(dataobj.outerHTML);
+    //console.log(conteudobj.outerHTML);
+    postobject.innerHTML = '';
+    postobject.innerHTML += tituloobj.outerHTML;
+    postobject.innerHTML += dataobj.outerHTML;
+    postobject.innerHTML += conteudobj.outerHTML;
+    console.log(postobject);
+    //provavelmente adicionar um lisciner para quando botão de post for clicado atualizar a pagina
+    //resultado final colocar um post novo com conteudo postado
+    //feedobject.innerHTML += postobject.outerHTML;
+
+}
+
+function doPublicar(){
+    let feed = document.getElementById("feed");
+    console.log(feed);
+    //let divPost = document.createElement("div")
+    //divPost.setAttribute("class", "post");
+    //console.log(divPost.outerHTML);
+    //feed.innerHTML = divPost.outerHTML;
+    //console.log(feed)
+
+
+    /*<div class="post">
+        <h1></h1>
+        <h4></h4>
+        <p></p> 
+        <p>ass: falcon</p>
+    </div> */
 }
